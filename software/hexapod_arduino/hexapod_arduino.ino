a7a3ed8f9d871db8a45ae1652293bb9b82ad4d28
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2024-06-18 15:20:05 -0400

    Update hexapod_arduino.ino

--- software/hexapod_arduino/hexapod_arduino.ino
+++ software/hexapod_arduino/hexapod_arduino.ino
@@ -1,70 +1,50 @@
+#include <Adafruit_PWMServoDriver.h>
 #include <ESP32Servo.h>
 #include <Wire.h>
-#include <Adafruit_PWMServoDriver.h>
 
 Adafruit_PWMServoDriver left_pwm = Adafruit_PWMServoDriver(0x40);
 Adafruit_PWMServoDriver right_pwm = Adafruit_PWMServoDriver(0x41);
 
-#define SERVOMIN  125  // Minimum value - previously 80
-#define SERVOMAX  575  // Maximum value - previously 1024
+#define SERVOMIN 125  // Minimum value, 0 deg
+#define SERVOMID 350  // Middle value, 90 deg
+#define SERVOMAX 575  // Maximum value, 180 deg
+
+int left_legs[3][3] = {{0, 1, 3}, {5, 7, 10}, {12, 14, 15}};
+int right_legs[3][3] = {{0, 1, 2}, {5, 8, 9}, {12, 14, 15}};
+
+// Offset to correct the installation error. Offset value is the number of ticks
+int left_offset_ticks[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+int right_offset_ticks[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+
+int left_flip[3][3] = {{1, 1, 1}, {1, 1, 1}, {1, 1, 1}};
+int right_flip[3][3] = {{1, 1, 1}, {1, 1, 1}, {1, 1, 1}};
 
 int pulselength;
 
-
-void setup()
-{
-
+void setup() {
   Serial.begin(115200);
 
   // Initialize the PCA9685 library
   left_pwm.begin();
-  left_pwm.setPWMFreq(60); // Set the PWM frequency of the PCA9685 (you may need to adjust this)
+  left_pwm.setPWMFreq(60);  // Set the PWM frequency of the PCA9685
 
   right_pwm.begin();
-  right_pwm.setPWMFreq(60); // Set the PWM frequency of the PCA9685 (you may need to adjust this)
-
-//  pulselength = map(90, 0, 180, SERVOMIN, SERVOMAX);
-//
-//  left_pwm.setPWM(0, 0, pulselength);
-
-  // Initialize all servos
-//  for (int i = 0; i < 4; i++)
-//  {
-//    for (int j = 0; j < 3; j++)
-//    {
-//      // Attach servo to PCA9685 channel
-//      pwm.setPWM(servo_pins[i][j],0,0); // Initialize servos at their center position
-//    }
-//  }
-//  center_servos();
-//  Serial.println("center");
-//  delay(2000);
+  right_pwm.setPWMFreq(60);  // Set the PWM frequency of the PCA9685
 }
 
-void loop()
-{
-  pulselength = map(90, 0, 180, SERVOMIN, SERVOMAX);
-  left_pwm.setPWM(0, 0, pulselength);
-  left_pwm.setPWM(1, 0, pulselength);
-  left_pwm.setPWM(3, 0, pulselength);
+void loop() {
+  // pulselength = map(90, 0, 180, SERVOMIN, SERVOMAX);
 
-  left_pwm.setPWM(5, 0, pulselength);
-  left_pwm.setPWM(7, 0, pulselength);
-  left_pwm.setPWM(10, 0, pulselength);
+  posture_calibration();
+}
 
-  left_pwm.setPWM(12, 0, pulselength);
-  left_pwm.setPWM(14, 0, pulselength);
-  left_pwm.setPWM(15, 0, pulselength);
-
-  right_pwm.setPWM(0, 0, pulselength);
-  right_pwm.setPWM(1, 0, pulselength);
-  right_pwm.setPWM(2, 0, pulselength);
-
-  right_pwm.setPWM(5, 0, pulselength);
-  right_pwm.setPWM(8, 0, pulselength);
-  right_pwm.setPWM(9, 0, pulselength);
-
-  right_pwm.setPWM(12, 0, pulselength);
-  right_pwm.setPWM(14, 0, pulselength);
-  right_pwm.setPWM(15, 0, pulselength);
+void posture_calibration() {
+  for (int leg_idx = 0; leg_idx < 3; leg_idx++) {
+    for (int joint_idx = 0; joint_idx < 3; joint_idx++) {
+      right_pwm.setPWM(right_legs[leg_idx][joint_idx], 0,
+                       SERVOMID + right_offset_ticks[leg_idx][joint_idx]);
+      left_pwm.setPWM(left_legs[leg_idx][joint_idx], 0,
+                      SERVOMID + left_offset_ticks[leg_idx][joint_idx]);
+    }
+  }
 }
