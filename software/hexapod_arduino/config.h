9faed90c52ce23511f63cdd25320169e2ae3a90c
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2024-07-02 13:24:29 -0400

    Update config.h

--- software/hexapod_arduino/config.h
+++ software/hexapod_arduino/config.h
@@ -66,7 +66,16 @@ static int left_legs[3][3] = {{0, 3, 1}, {7, 5, 10}, {15, 12, 14}};
 static int right_legs[3][3] = {{15, 12, 14}, {8, 9, 5}, {0, 2, 1}};
 
 // Offset to correct the installation error. Offset value is the number of ticks
-static int left_offset_ticks[3][3] = {{ -5, -20, 6}, { -15, 0, 14}, {20, 6, 0}};
-static int right_offset_ticks[3][3] = {{20, 15, 0}, { -15, 20, -20}, { -10, -8, 10}};
+static int left_offset_ticks[3][3] = {{-5, -20, 6}, {-15, 0, 14}, {20, 6, 0}};
+static int right_offset_ticks[3][3] = {
+    {20, 15, 0}, {-15, 20, -20}, {-10, -8, 10}};
 
-#endif // CONFIG_H
+/** WiFi Configurations */
+#ifndef APSSID
+#define APSSID "hexapod"
+#define APPSK "hexapod_1234"
+#endif
+
+#define UDP_PORT 1234  // local port to listen on
+
+#endif  // CONFIG_H
