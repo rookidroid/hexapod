d0c69d4874175e9e608b9c8b5e5e01ad9c3796d2
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2025-05-28 21:37:29 -0400

    add a flag to set calibration mode

--- software/hexapod_esp32/hexapod_esp32.ino
+++ software/hexapod_esp32/hexapod_esp32.ino
@@ -61,6 +61,8 @@ const char *ssid = APSSID;
 const char *password = APPSK;
 AsyncUDP udp_socket;
 
+bool calibration_mode = false;
+
 bool ota_mode = true;
 
 /**
@@ -81,38 +83,36 @@ void setup() {
   Serial.println(myIP);
 
   ArduinoOTA
-  .onStart([]() {
-    String type;
-    if (ArduinoOTA.getCommand() == U_FLASH) {
-      type = "sketch";
-    } else {  // U_SPIFFS
-      type = "filesystem";
-    }
+      .onStart([]() {
+        String type;
+        if (ArduinoOTA.getCommand() == U_FLASH) {
+          type = "sketch";
+        } else {  // U_SPIFFS
+          type = "filesystem";
+        }
 
-    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS
-    // using SPIFFS.end()
-    Serial.println("Start updating " + type);
-  })
-  .onEnd([]() {
-    Serial.println("\nEnd");
-  })
-  .onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  })
-  .onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) {
-      Serial.println("Auth Failed");
-    } else if (error == OTA_BEGIN_ERROR) {
-      Serial.println("Begin Failed");
-    } else if (error == OTA_CONNECT_ERROR) {
-      Serial.println("Connect Failed");
-    } else if (error == OTA_RECEIVE_ERROR) {
-      Serial.println("Receive Failed");
-    } else if (error == OTA_END_ERROR) {
-      Serial.println("End Failed");
-    }
-  });
+        // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS
+        // using SPIFFS.end()
+        Serial.println("Start updating " + type);
+      })
+      .onEnd([]() { Serial.println("\nEnd"); })
+      .onProgress([](unsigned int progress, unsigned int total) {
+        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+      })
+      .onError([](ota_error_t error) {
+        Serial.printf("Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR) {
+          Serial.println("Auth Failed");
+        } else if (error == OTA_BEGIN_ERROR) {
+          Serial.println("Begin Failed");
+        } else if (error == OTA_CONNECT_ERROR) {
+          Serial.println("Connect Failed");
+        } else if (error == OTA_RECEIVE_ERROR) {
+          Serial.println("Receive Failed");
+        } else if (error == OTA_END_ERROR) {
+          Serial.println("End Failed");
+        }
+      });
 
   ArduinoOTA.begin();
 
@@ -130,7 +130,7 @@ void setup() {
       Serial.print("UDP Packet Type: ");
       Serial.print(packet.isBroadcast()   ? "Broadcast"
                    : packet.isMulticast() ? "Multicast"
-                   : "Unicast");
+                                          : "Unicast");
       Serial.print(", From: ");
       Serial.print(packet.remoteIP());
       Serial.print(":");
@@ -209,7 +209,11 @@ void setup() {
   digitalWrite(19, LOW);
   digitalWrite(26, LOW);
 
-  boot_up_motion(lut_standup_length, lut_standup);
+  if (calibration_mode) {
+    posture_calibration();
+  } else {
+    boot_up_motion(lut_standup_length, lut_standup);
+  }
 
   //  posture_calibration();
 }
@@ -223,44 +227,46 @@ void setup() {
    `ArduinoOTA.handle()`.
 */
 void loop() {
-  if (next_motion == MotionMode::Mode_Walk_0) {
-    exec_motion(lut_walk_0_length, lut_walk_0);
-  } else if (next_motion == MotionMode::Mode_Walk_180) {
-    exec_motion(lut_walk_180_length, lut_walk_180);
-  } else if (next_motion == MotionMode::Mode_Walk_R45) {
-    exec_motion(lut_walk_r45_length, lut_walk_r45);
-  } else if (next_motion == MotionMode::Mode_Walk_R90) {
-    exec_motion(lut_walk_r90_length, lut_walk_r90);
-  } else if (next_motion == MotionMode::Mode_Walk_R135) {
-    exec_motion(lut_walk_r135_length, lut_walk_r135);
-  } else if (next_motion == MotionMode::Mode_Walk_L45) {
-    exec_motion(lut_walk_l45_length, lut_walk_l45);
-  } else if (next_motion == MotionMode::Mode_Walk_L90) {
-    exec_motion(lut_walk_l90_length, lut_walk_l90);
-  } else if (next_motion == MotionMode::Mode_Walk_L135) {
-    exec_motion(lut_walk_l135_length, lut_walk_l135);
-  } else if (next_motion == MotionMode::Mode_Fast_Forward) {
-    exec_motion(lut_fast_forward_length, lut_fast_forward);
-  } else if (next_motion == MotionMode::Mode_Fast_Backward) {
-    exec_motion(lut_fast_backward_length, lut_fast_backward);
-  } else if (next_motion == MotionMode::Mode_Turn_Left) {
-    exec_motion(lut_turn_left_length, lut_turn_left);
-  } else if (next_motion == MotionMode::Mode_Turn_Right) {
-    exec_motion(lut_turn_right_length, lut_turn_right);
-  } else if (next_motion == MotionMode::Mode_Climb_Forward) {
-    exec_motion(lut_climb_forward_length, lut_climb_forward);
-  } else if (next_motion == MotionMode::Mode_Climb_Backward) {
-    exec_motion(lut_climb_backward_length, lut_climb_backward);
-  } else if (next_motion == MotionMode::Mode_Rotate_X) {
-    exec_motion(lut_rotate_x_length, lut_rotate_x);
-  } else if (next_motion == MotionMode::Mode_Rotate_Y) {
-    exec_motion(lut_rotate_y_length, lut_rotate_y);
-  } else if (next_motion == MotionMode::Mode_Rotate_Z) {
-    exec_motion(lut_rotate_z_length, lut_rotate_z);
-  } else if (next_motion == MotionMode::Mode_Twist) {
-    exec_motion(lut_twist_length, lut_twist);
-  } else {
-    exec_motion(lut_standby_length, lut_standby);
+  if (!calibration_mode) {
+    if (next_motion == MotionMode::Mode_Walk_0) {
+      exec_motion(lut_walk_0_length, lut_walk_0);
+    } else if (next_motion == MotionMode::Mode_Walk_180) {
+      exec_motion(lut_walk_180_length, lut_walk_180);
+    } else if (next_motion == MotionMode::Mode_Walk_R45) {
+      exec_motion(lut_walk_r45_length, lut_walk_r45);
+    } else if (next_motion == MotionMode::Mode_Walk_R90) {
+      exec_motion(lut_walk_r90_length, lut_walk_r90);
+    } else if (next_motion == MotionMode::Mode_Walk_R135) {
+      exec_motion(lut_walk_r135_length, lut_walk_r135);
+    } else if (next_motion == MotionMode::Mode_Walk_L45) {
+      exec_motion(lut_walk_l45_length, lut_walk_l45);
+    } else if (next_motion == MotionMode::Mode_Walk_L90) {
+      exec_motion(lut_walk_l90_length, lut_walk_l90);
+    } else if (next_motion == MotionMode::Mode_Walk_L135) {
+      exec_motion(lut_walk_l135_length, lut_walk_l135);
+    } else if (next_motion == MotionMode::Mode_Fast_Forward) {
+      exec_motion(lut_fast_forward_length, lut_fast_forward);
+    } else if (next_motion == MotionMode::Mode_Fast_Backward) {
+      exec_motion(lut_fast_backward_length, lut_fast_backward);
+    } else if (next_motion == MotionMode::Mode_Turn_Left) {
+      exec_motion(lut_turn_left_length, lut_turn_left);
+    } else if (next_motion == MotionMode::Mode_Turn_Right) {
+      exec_motion(lut_turn_right_length, lut_turn_right);
+    } else if (next_motion == MotionMode::Mode_Climb_Forward) {
+      exec_motion(lut_climb_forward_length, lut_climb_forward);
+    } else if (next_motion == MotionMode::Mode_Climb_Backward) {
+      exec_motion(lut_climb_backward_length, lut_climb_backward);
+    } else if (next_motion == MotionMode::Mode_Rotate_X) {
+      exec_motion(lut_rotate_x_length, lut_rotate_x);
+    } else if (next_motion == MotionMode::Mode_Rotate_Y) {
+      exec_motion(lut_rotate_y_length, lut_rotate_y);
+    } else if (next_motion == MotionMode::Mode_Rotate_Z) {
+      exec_motion(lut_rotate_z_length, lut_rotate_z);
+    } else if (next_motion == MotionMode::Mode_Twist) {
+      exec_motion(lut_twist_length, lut_twist);
+    } else {
+      exec_motion(lut_standby_length, lut_standby);
+    }
   }
 
   if (ota_mode) {
@@ -290,13 +296,13 @@ void posture_calibration() {
 void boot_up_motion(int lut_size, int lut[][6][3]) {
   for (int leg_idx = 0; leg_idx < 3; leg_idx++) {
     for (int joint_idx = 0; joint_idx < 3; joint_idx++) {
-      right_pwm.setPWM(right_legs[leg_idx][joint_idx], 0,
-                       lut[0][leg_idx][joint_idx] +
-                       right_offset_ticks[leg_idx][joint_idx]);
+      right_pwm.setPWM(
+          right_legs[leg_idx][joint_idx], 0,
+          lut[0][leg_idx][joint_idx] + right_offset_ticks[leg_idx][joint_idx]);
       delay(50);
       left_pwm.setPWM(left_legs[leg_idx][joint_idx], 0,
                       lut[0][leg_idx + 3][joint_idx] +
-                      left_offset_ticks[leg_idx][joint_idx]);
+                          left_offset_ticks[leg_idx][joint_idx]);
       delay(50);
     }
   }
@@ -306,10 +312,10 @@ void boot_up_motion(int lut_size, int lut[][6][3]) {
       for (int joint_idx = 0; joint_idx < 3; joint_idx++) {
         right_pwm.setPWM(right_legs[leg_idx][joint_idx], 0,
                          lut[lut_idx][leg_idx][joint_idx] +
-                         right_offset_ticks[leg_idx][joint_idx]);
+                             right_offset_ticks[leg_idx][joint_idx]);
         left_pwm.setPWM(left_legs[leg_idx][joint_idx], 0,
                         lut[lut_idx][leg_idx + 3][joint_idx] +
-                        left_offset_ticks[leg_idx][joint_idx]);
+                            left_offset_ticks[leg_idx][joint_idx]);
       }
     }
     delay(DELAY_MS);
@@ -339,10 +345,10 @@ void exec_motion(int lut_size, int lut[][6][3]) {
       for (int joint_idx = 0; joint_idx < 3; joint_idx++) {
         right_pwm.setPWM(right_legs[leg_idx][joint_idx], 0,
                          lut[lut_idx][leg_idx][joint_idx] +
-                         right_offset_ticks[leg_idx][joint_idx]);
+                             right_offset_ticks[leg_idx][joint_idx]);
         left_pwm.setPWM(left_legs[leg_idx][joint_idx], 0,
                         lut[lut_idx][leg_idx + 3][joint_idx] +
-                        left_offset_ticks[leg_idx][joint_idx]);
+                            left_offset_ticks[leg_idx][joint_idx]);
       }
     }
 
@@ -386,7 +392,7 @@ void exec_transition(int start_pos[][6][3], int start_pos_idx,
       diff = end_pos[end_pos_idx][leg_idx][joint_idx] -
              start_pos[start_pos_idx][leg_idx][joint_idx];
       current_pos[leg_idx][joint_idx] =
-        start_pos[start_pos_idx][leg_idx][joint_idx];
+          start_pos[start_pos_idx][leg_idx][joint_idx];
       if (diff < 0) {
         signed_ticks[leg_idx][joint_idx] = -tick_step;
       } else {
@@ -405,25 +411,25 @@ void exec_transition(int start_pos[][6][3], int start_pos_idx,
                                             signed_ticks[leg_idx][joint_idx];
         } else {
           current_pos[leg_idx][joint_idx] =
-            end_pos[end_pos_idx][leg_idx][joint_idx];
+              end_pos[end_pos_idx][leg_idx][joint_idx];
         }
 
         if (abs(current_pos[leg_idx + 3][joint_idx] -
                 end_pos[end_pos_idx][leg_idx + 3][joint_idx]) > tick_step) {
           current_pos[leg_idx + 3][joint_idx] =
-            current_pos[leg_idx + 3][joint_idx] +
-            signed_ticks[leg_idx + 3][joint_idx];
+              current_pos[leg_idx + 3][joint_idx] +
+              signed_ticks[leg_idx + 3][joint_idx];
         } else {
           current_pos[leg_idx + 3][joint_idx] =
-            end_pos[end_pos_idx][leg_idx + 3][joint_idx];
+              end_pos[end_pos_idx][leg_idx + 3][joint_idx];
         }
 
         right_pwm.setPWM(right_legs[leg_idx][joint_idx], 0,
                          current_pos[leg_idx][joint_idx] +
-                         right_offset_ticks[leg_idx][joint_idx]);
+                             right_offset_ticks[leg_idx][joint_idx]);
         left_pwm.setPWM(left_legs[leg_idx][joint_idx], 0,
                         current_pos[leg_idx + 3][joint_idx] +
-                        left_offset_ticks[leg_idx][joint_idx]);
+                            left_offset_ticks[leg_idx][joint_idx]);
       }
     }
   }
