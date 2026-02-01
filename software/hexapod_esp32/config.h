fc772a032bf80eb1da8d470f673a997d0cbcfee0
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2026-02-01 10:48:15 -0500

    Update 3D prints and adjust servo offsets
    Replace binary 3MF/STL assets for foot and body side, and update hexapod calibration offsets. In software/hexapod_esp32/config.h the left_offset_ticks and right_offset_ticks were changed from all zeros to calibrated values to correct installation misalignment ({ {15,10,15},{5,10,15},{-8,5,-5} } and { {0,0,-5},{-5,-10,-10},{-5,-10,-15} } respectively).

--- software/hexapod_esp32/config.h
+++ software/hexapod_esp32/config.h
@@ -49,8 +49,8 @@ static int left_legs[3][3] = { { 1, 2, 3 }, { 5, 6, 7 }, { 9, 8, 10 } };
 static int right_legs[3][3] = { { 10, 9, 8 }, { 13, 14, 15 }, { 7, 6, 5 } };
 
 // Offset to correct the installation error. Offset value is the number of ticks
-static int left_offset_ticks[3][3] = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
-static int right_offset_ticks[3][3] = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
+static int left_offset_ticks[3][3] = { { 15, 10, 15 }, { 5, 10, 15 }, { -8, 5, -5 } };
+static int right_offset_ticks[3][3] = { { 0, 0, -5 }, { -5, -10, -10 }, { -5, -10, -15 } };
 
 /** WiFi Configurations */
 #ifndef APSSID
